a397ab474c4476c594884c8ca406fd39b1945062
Thierry Baribaud <thierry.baribaud@gmail.com>
2016-05-25 00:45:07 +0200

    Evolution au 24/05/2016
    - Importation de la classe GetArgs et de l'exception GetArgsException du projet ExpCalls. 
    - Adaptation de la classe ExpAgency en conséquence.

--- src/expagency/ExpAgency.java
+++ src/expagency/ExpAgency.java
@@ -20,190 +20,8 @@ import liba2pi.DBServerException;
 public class ExpAgency {
 
     /**
-     * SourceServer : prod pour le serveur de production, dev pour le serveur de
-     * développement. Valeur par défaut : dev.
-     */
-    private String SourceServer = "dev";
-
-    /**
-     * Unum : Référence du client.
-     * Valeur par défaut : doit être spécifié en ligne de commande.
-     */
-    private int unum;
-
-    /**
-     * FileOut : fichier qui recevra les résultats du chargement.
-     * Valeur par défaut : agences.xml.
-     */
-    private String FileOut = "agences.xml";
-
-    /**
-     * debugMode : fonctionnement du programme en mode debug (true/false).
-     * Valeur par défaut : false.
-     */
-    private boolean debugMode = false;
-
-    /**
-     * testMode : fonctionnement du programme en mode test (true/false). Valeur
-     * par défaut : false.
-     */
-    private boolean testMode = false;
-
-  /**
-   * @return SourceServer : retourne la valeur pour le serveur source.
-   */
-  private String getSourceServer() {
-    return(SourceServer);
-  }
-
-  /**
-   * @return FileOut : retourne le nom du fichier où envoyer les résultats.
-   */
-  private String getFileOut() {
-    return(FileOut);
-  }
-
-  /**
-   * @return Unum : retourne la référence du client.
-   */
-  private int getUnum() {
-    return(unum);
-  }
-
-  /**
-   * @return daemonMode : retourne le mode de fonctionnement debug.
-   */
-  private boolean getDebugMode() {
-    return(debugMode);
-  }
-  
-  /**
-   * @return testMode : retourne le mode de fonctionnement test.
-   */
-  private boolean getTestMode() {
-    return(testMode);
-  }
-  
-  /**
-   * @param MySourceServer : définit le serveur source.
-   */
-  private void setSourceServer(String MySourceServer) {
-    this.SourceServer = MySourceServer;
-  }
-
-  /**
-   * @param MyFileOut : définit le fichier où envoyer les résultats.
-   */
-  private void setFileOut(String MyFileOut) {
-    this.FileOut = MyFileOut;
-  }
-
-  /**
-   * @param MyUnum : définit la référence client.
-   */
-  private void setUnum(int myUnum) {
-    this.unum = myUnum;
-  }
-
-    /**
-    * debugMode : fonctionnement du programme en mode debug (true/false).
-   */
-  private void setDebugMode(boolean myDebugMode) {
-    this.debugMode = myDebugMode;
-  }
-
-  /**
-    * testMode : fonctionnement du programme en mode test (true/false).
-   */
-  private void setTestMode(boolean myTestMode) {
-    this.testMode = myTestMode;
-  }
-
-  /**
-   * Récupère les arguments de la ligne de commande.
-   * @param Args arguments de la ligne de commande.
-   * @throws ImpAgencyException en cas d'erreur.
-   */
-    private void getArgs(String[] Args) throws ExpAgencyException {
-
-        String[] Errmsg = {"Erreur n°1 : Mauvaise source de données",
-            "Erreur n°2 : Mauvaise référence client",
-            "Erreur n°3 : Mauvais fichier résultat",
-            "Erreur n°4 : Mauvais argument"};
-        String ErrorValue = "";
-        int errNo = Errmsg.length;
-        int i;
-        int n;
-        int ip1;
-
-        n = Args.length;
-
-        System.out.println("nargs=" + n);
-//    for(i=0; i<n; i++) System.out.println("args["+i+"]="+Args[i]);
-        i = 0;
-        while (i < n) {
-//            System.out.println("args[" + i + "]=" + Args[i]);
-            ip1 = i + 1;
-            if (Args[i].equals("-dbserver")) {
-                if (ip1 < n) {
-                    if (Args[ip1].equals("dev") || Args[ip1].equals("prod") || Args[ip1].equals("mysql")) {
-                        setSourceServer(Args[ip1]);
-                    } else {
-                        errNo = 0;
-                        ErrorValue = Args[ip1];
-                    }
-                    i = ip1;
-                } else {
-                    errNo = 0;
-                    ErrorValue = "base de données non définie";
-                }
-            } else if (Args[i].equals("-u")) {
-                if (ip1 < n) {
-                    try {
-                        setUnum(Integer.parseInt(Args[ip1]));
-                        i = ip1;
-                    }
-                    catch (Exception MyException) {
-                        errNo = 1;
-                        ErrorValue = "La référence client doit être numérique";
-                    }
-                } else {
-                    errNo = 1;
-                    ErrorValue = "référence client non définie";
-                }
-            } else if (Args[i].equals("-o")) {
-                if (ip1 < n) {
-                    setFileOut(Args[ip1]);
-                    i = ip1;
-                } else {
-                    errNo = 2;
-                    ErrorValue = "nom de fichier non défini";
-                }
-            } else if (Args[i].equals("-d")) {
-                setDebugMode(true);
-            } else if (Args[i].equals("-t")) {
-                setTestMode(true);
-            } else {
-                errNo = 3;
-                ErrorValue = Args[i];
-            }
-            i++;
-        }
-//        System.out.println("errNo=" + errNo);
-        if (errNo != Errmsg.length) {
-            throw new ExpAgencyException(Errmsg[errNo] + " : " + ErrorValue);
-        }
-    }
-
-    /**
      * Les arguments en ligne de commande permettent de changer le mode de 
-     * fonctionnement.
-     * -u unum : identifiant du service d'urgence (obligatoire).
-     * -o fichier : fichier vers lequel exporter les données de l'agence 
-     *              (optionnel, nom par défaut agences.xml).
-     * -d : le programme fonctionne en mode débug, il est plus verbeux (optionnel).
-     * -t : le programme fonctionne en mode de test, les transactions en base 
-     *      de données ne sont pas exécutées (optionnel).
+     * fonctionnement. Voir GetArgs pour plus de détails.
      * @param Args arguments de la ligne de commande. 
      * @throws expagency.ExpAgencyException 
      * @throws java.io.IOException 
@@ -214,6 +32,7 @@ public class ExpAgency {
         FagencyDAO MyFagencyDAO;
         AgencyXMLDocument MyXMLDocument;
 
+        GetArgs MyArgs;
         ApplicationProperties MyApplicationProperties;
         DBServer MyDBServer;
         DBManager MyDBManager;
@@ -223,47 +42,42 @@ public class ExpAgency {
 
         // On récupère les arguments de la ligne de commande.
         System.out.println("Récupération des arguments en ligne de commande ...");
-        getArgs(Args);
-
-        System.out.println("Lecture du fichier de paramètres ...");
-        MyApplicationProperties = new ApplicationProperties("ExpAgencyPublic.prop");
-
-        System.out.println("Lecture des paramètres de base de données ...");
-        MyDBServer = new DBServer(getSourceServer(), MyApplicationProperties);
-        System.out.println("  " + MyDBServer);
-
-        MyXMLDocument = new AgencyXMLDocument("agences", "agences_plat.xsd");
-
         try {
+            MyArgs = new GetArgs(Args);
+            System.out.println(MyArgs);
+
+            System.out.println("Lecture du fichier de paramètres ...");
+            MyApplicationProperties = new ApplicationProperties("ExpAgencyPublic.prop");
+
+            System.out.println("Lecture des paramètres de base de données ...");
+            MyDBServer = new DBServer(MyArgs.getSourceServer(), MyApplicationProperties);
+            System.out.println("  " + MyDBServer);
+
+            MyXMLDocument = new AgencyXMLDocument("agences", "agences.xsd");
+
             MyDBManager = new DBManager(MyDBServer);
 
-            MyFagencyDAO = new FagencyDAO(MyDBManager.getConnection(), unum, MyA6name);
+            MyFagencyDAO = new FagencyDAO(MyDBManager.getConnection(), MyArgs.getUnum(), MyA6name);
             i = 0;
             while ((MyFagency = MyFagencyDAO.select()) != null) {
                 i++;
                 System.out.println("Fagency(" + i + ")=" + MyFagency);
-//                MyXMLDocument.AddToXMLDocument(MyFagency);
                 MyXMLDocument.AddToXMLDocument(MyFagency);
             }
-        } catch (ClassNotFoundException ex) {
-            Logger.getLogger(ExpAgency.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (SQLException ex) {
-            Logger.getLogger(ExpAgency.class.getName()).log(Level.SEVERE, null, ex);
+            MyXMLDocument.FinalizeXMLDocument(MyArgs.getFileOut());
+        } catch (GetArgsException MyException) {
+            Logger.getLogger(ExpAgency.class.getName()).log(Level.SEVERE, null, MyException);
+            GetArgs.usage();
+        } catch (ClassNotFoundException MyException) {
+            Logger.getLogger(ExpAgency.class.getName()).log(Level.SEVERE, null, MyException);
+        } catch (SQLException MyException) {
+            Logger.getLogger(ExpAgency.class.getName()).log(Level.SEVERE, null, MyException);
         }
-
-        MyXMLDocument.FinalizeXMLDocument(getFileOut());
     }
 
     /**
-     * Methode pour lancer le programme.
      * Les arguments en ligne de commande permettent de changer le mode de 
-     * fonctionnement.
-     * -u unum : identifiant du service d'urgence (obligatoire).
-     * -o fichier : fichier vers lequel exporter les données de l'agence 
-     *              (optionnel, nom par défaut agences.xml).
-     * -d : le programme fonctionne en mode débug, il est plus verbeux (optionnel).
-     * -t : le programme fonctionne en mode de test, les transactions en base 
-     *      de données ne sont pas exécutées (optionnel).
+     * fonctionnement. Voir GetArgs pour plus de détails.
      * @param Args arguments de la ligne de commande. 
      */
     public static void main(String[] Args){
---
+++ src/expagency/GetArgs.java
@@ -0,0 +1,210 @@
+/*
+ * Cette classe sert à vérifier et à récupérer les arguments passés en ligne de
+ * commande à un programme.
+ * @version Mai 2016.
+ * @author Thierry Baribaud.
+ */
+package expagency;
+
+import java.util.Date;
+
+public class GetArgs {
+
+    /**
+     * SourceServer : prod pour le serveur de production, dev pour le serveur de
+     * développement. Valeur par défaut : dev.
+     */
+    private String SourceServer = "dev";
+
+    /**
+     * Unum : Référence du client. Valeur par défaut : doit être spécifié en
+     * ligne de commande.
+     */
+    private int unum = 0;
+
+    /**
+     * FileOut : fichier qui recevra les résultats de l'export. Valeur par
+     * défaut : agences.xml.
+     */
+    private String FileOut = "agences.xml";
+
+    
+    
+    /**
+     * debugMode : fonctionnement du programme en mode debug (true/false).
+     * Valeur par défaut : false.
+     */
+    private boolean debugMode = false;
+
+    /**
+     * testMode : fonctionnement du programme en mode test (true/false). Valeur
+     * par défaut : false.
+     */
+    private boolean testMode = false;
+
+    /**
+     * @return SourceServer : retourne la valeur pour le serveur source.
+     */
+    public String getSourceServer() {
+        return (SourceServer);
+    }
+
+    /**
+     * @return FileOut : retourne le nom du fichier où envoyer les résultats.
+     */
+    public String getFileOut() {
+        return (FileOut);
+    }
+
+    /**
+     * @return Unum : retourne la référence du client.
+     */
+    public int getUnum() {
+        return (unum);
+    }
+
+    /**
+     * @return debugMode : retourne le mode de fonctionnement debug.
+     */
+    public boolean getDebugMode() {
+        return (debugMode);
+    }
+
+    /**
+     * @return testMode : retourne le mode de fonctionnement test.
+     */
+    public boolean getTestMode() {
+        return (testMode);
+    }
+
+    /**
+     * @param SourceServer : définit le serveur source.
+     */
+    public void setSourceServer(String SourceServer) {
+        this.SourceServer = SourceServer;
+    }
+
+    /**
+     * @param FileOut : définit le fichier où envoyer les résultats.
+     */
+    public void setFileOut(String FileOut) {
+        this.FileOut = FileOut;
+    }
+
+    /**
+     * @param unum : définit la référence client.
+     */
+    public void setUnum(int unum) {
+        this.unum = unum;
+    }
+
+    /**
+     * @param debugMode : fonctionnement du programme en mode debug (true/false).
+     */
+    public void setDebugMode(boolean debugMode) {
+        this.debugMode = debugMode;
+    }
+
+    /**
+     * @param testMode : fonctionnement du programme en mode test (true/false).
+     */
+    public void setTestMode(boolean testMode) {
+        this.testMode = testMode;
+    }
+
+    /**
+     * <p>Les arguments en ligne de commande permettent de changer le mode de 
+     * fonctionnement.</p><ul>
+     * <li>-dbserver : référence à la base de donnée, par défaut fait référence 
+     * à la base de développement,  cf. fichier de paramètres 
+     * <i>myDatabases.prop</i> (optionnel)</li>
+     * <li>-u unum : identifiant du service d'urgence (obligatoire).</li>
+     * <li>-o fichier : fichier vers lequel exporter les données de l'agence, 
+     * nom par défaut <i>agences.xml</i>(optionnel).</li>
+     * <li>-d : le programme fonctionne en mode débug le rendant plus verbeux, 
+     * désactivé par défaut (optionnel).</li>
+     * <li>-t : le programme fonctionne en mode de test, les transactions en base 
+     *      de données ne sont pas exécutées, désactivé par défaut (optionnel).</li>
+     * </ul>
+     * @param Args arguments de la ligne de commande. 
+     * @throws GetArgsException 
+     */
+    public GetArgs(String Args[]) throws GetArgsException {
+
+        int i;
+        int n;
+        int ip1;
+        Date MyDate;
+
+        // Demande une analyse d'une date valide
+        n = Args.length;
+
+        System.out.println("nargs=" + n);
+//    for(i=0; i<n; i++) System.out.println("args["+i+"]="+Args[i]);
+        i = 0;
+        while (i < n) {
+//            System.out.println("args[" + i + "]=" + Args[i]);
+            ip1 = i + 1;
+            if (Args[i].equals("-dbserver")) {
+                if (ip1 < n) {
+                    if (Args[ip1].equals("dev") || Args[ip1].equals("prod") || Args[ip1].equals("mysql")) {
+                        setSourceServer(Args[ip1]);
+                    } else {
+                        throw new GetArgsException("Mauvaise source de données : " + Args[ip1]);
+                    }
+                    i = ip1;
+                } else {
+                    throw new GetArgsException("Base de données non définie");
+                }
+            } else if (Args[i].equals("-u")) {
+                if (ip1 < n) {
+                    try {
+                        setUnum(Integer.parseInt(Args[ip1]));
+                        i = ip1;
+                    } catch (Exception MyException) {
+                        throw new GetArgsException("La référence client doit être numérique : " + Args[ip1]);
+                    }
+                } else {
+                    throw new GetArgsException("Référence client non définie");
+                }
+            } else if (Args[i].equals("-o")) {
+                if (ip1 < n) {
+                    setFileOut(Args[ip1]);
+                    i = ip1;
+                } else {
+                    throw new GetArgsException("Nom de fichier non défini");
+                }
+            } else if (Args[i].equals("-d")) {
+                setDebugMode(true);
+            } else if (Args[i].equals("-t")) {
+                setTestMode(true);
+            } else {
+                throw new GetArgsException("Mauvais argument : " + Args[i]);
+            }
+            i++;
+        }
+    }
+        
+    /**
+     * Affiche le mode d'utilisation du programme.
+     */
+    public static void usage() {
+        System.out.println("Usage : java ExpCalls -dbserver prod -u unum " +
+                           " [-o fichier.xml] [-d] [-t]");
+    }
+    
+    /**
+     * Affiche le contenu de GetArgs.
+     * @return retourne le contenu de GetArgs.
+     */
+    @Override
+    public String toString() {
+    return this.getClass().getName() +
+           " : {dbServer=" + SourceServer + 
+           ", unum=" + unum + 
+           ", fichier=" + FileOut +
+           ", debugMode=" + debugMode + 
+           ", testMode=" + testMode +
+           "}";
+    }
+}
